b37b86910e1e8104a4978e28286eeea92f484a5b
Kreijstal <rainb@tfwno.gf>
2024-11-26 20:20:35 +0100

    should fail ONLY on gnullbm

--- openvaf/target/src/ucrt.c
+++ openvaf/target/src/ucrt.c
@@ -1,30 +1,24 @@
 #define NULL ((void *)0)
-#define _CRT_INTERNAL_PRINTF_STANDARD_SNPRINTF_BEHAVIOR 0x0002ULL
+#define _CRT_INTERNAL_PRINTF_STANDARD_SNPRINTF_BEHAVIOR        0x0002ULL
 
 #ifdef __MINGW32__
 #include <stddef.h>
 #else
-#ifndef _MSC_VER
 typedef void* _locale_t;
+typedef char* va_list;
 #endif
-#include <stdarg.h>
-#endif
+//We call this gnullvm
+#if defined(__clang__) && defined(__MINGW32__) 
 
-#ifdef _MSC_VER
+#else
 int __cdecl __stdio_common_vsprintf(unsigned __int64 options, char *str, size_t len, const char *format, _locale_t locale, va_list valist);
-#endif
-
 int __cdecl snprintf (char * __restrict__ __stream, size_t __n, const char * __restrict__ __format, ...)
 {
-    __builtin_va_list ap;
-    int ret;
-    __builtin_va_start(ap, __format);
-#ifdef _MSC_VER
-    ret = __stdio_common_vsprintf(_CRT_INTERNAL_PRINTF_STANDARD_SNPRINTF_BEHAVIOR, __stream, __n, __format, NULL, ap);
-#else
-    ret = vsnprintf(__stream, __n, __format, ap);
-#endif
-    __builtin_va_end(ap);
-    return ret;
+  __builtin_va_list ap;
+  int ret;
+  __builtin_va_start(ap, __format);
+  ret = __stdio_common_vsprintf(_CRT_INTERNAL_PRINTF_STANDARD_SNPRINTF_BEHAVIOR, __stream, __n, __format, NULL, ap);
+  __builtin_va_end(ap);
+  return ret;
 }
-
+#endif
